ee362b92ce09dab98a23175fda5fae5d0a789f42
Le Dai <dl123@mail.ustc.edu.cn>
2022-02-27 01:00:14 +0800

    feat: add camera and basic snake

--- crates/snake_plugin/src/lib.rs
+++ crates/snake_plugin/src/lib.rs
@@ -1,33 +1,24 @@
 use bevy::prelude::*;
 
-#[derive(Component)]
-struct Person;
+pub struct SnakePlugin;
 
-#[derive(Component)]
-struct Name(String);
-
-fn add_people(mut commands: Commands) {
-    commands.spawn().insert(Person).insert(Name("Elaina Proctor".to_string()));
-    commands.spawn().insert(Person).insert(Name("Renzo Hume".to_string()));
-    commands.spawn().insert(Person).insert(Name("Zayna Nieves".to_string()));
-}
-
-fn hello_world() {
-    println!("hello world!");
-}
-
-fn greet_people(query: Query<&Name, With<Person>>) {
-    for name in query.iter() {
-        println!("hello {}!", name.0);
-    }
-}
-
-pub struct HelloPlugin;
-
-impl Plugin for HelloPlugin {
+impl Plugin for SnakePlugin {
     fn build(&self, app: &mut App) {
-        app.add_startup_system(add_people)
-           .add_system(hello_world)
-           .add_system(greet_people);
+        app.add_startup_system(setup);
     }
+}
+
+/// Create 2d camera, initial wall and snake.
+fn setup(mut commands: Commands) {
+    // add 2d camera
+    commands.spawn_bundle(OrthographicCameraBundle::new_2d());
+    // add snake
+    commands.spawn_bundle(SpriteBundle {
+        sprite: Sprite {
+            color: Color::rgb(0.25, 0.25, 0.75),
+            custom_size: Some(Vec2::new(50.0, 50.0)),
+            ..Default::default()
+        },
+        ..Default::default()
+    });
 }
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 use bevy::prelude::*;
-use snake_plugin::HelloPlugin;
+use snake_plugin::SnakePlugin;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .insert_resource(ClearColor(Color::BLACK))
-        .add_plugin(HelloPlugin)
+        .add_plugin(SnakePlugin)
         .run();
 }
